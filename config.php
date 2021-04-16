6bd627fbe2fdaccb7c015ad6df7973e60052b598
simon <sepkap123@gmail.com>
2021-04-16 21:42:06 +0300

    initial commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'test2');
+$link = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+if ($link->connect_errno) {
+    printf("Connect failed: %s\n", $link->connect_error);
+    exit();
+}
---
+++ delete.php
@@ -0,0 +1,19 @@
+<?php
+if (isset($_GET["id"]) && !empty($_GET["id"])) {
+    require_once "config.php";
+    $sql = "DELETE FROM Users WHERE id = ?";
+    if ($stmt = $link->prepare($sql)) {
+        $stmt->bind_param("i", $_GET["id"]);
+        if ($stmt->execute()) {
+            header("location: index.php");
+            exit();
+        } else {
+            echo "Error! Please try again later.";
+        }
+    }
+    $stmt->close();
+    $link->close();
+} else {
+    echo "Error! Please try again later.";
+}
+?>
